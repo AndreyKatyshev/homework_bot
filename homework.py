4915cf67ef41adbc3ad6703499aae930765d4e73
Андрей Катышев <Andrey-Katyshev@mail.ru>
2022-05-31 21:54:00 +0300

    commit after second review

--- homework.py
+++ homework.py
@@ -7,34 +7,23 @@ import time
 
 from dotenv import load_dotenv
 from http import HTTPStatus
-# from logging.handlers import RotatingFileHandler
-from pprint import pprint
+from logging.handlers import RotatingFileHandler
 
-from exceptions import StatusCodeException, NotForSendException
+from exceptions import NotForSendException, StatusCodeException, TypeException
 
 
 load_dotenv()
-# logger = logging.getLogger(__name__)
-# logger.setLevel(logging.INFO)
-# handler = RotatingFileHandler(
-#     'program.log', maxBytes=50000000, backupCount=5)
-# logger.addHandler(handler)
 
-# можно ли эти настройки перенести
-# просто за условие if __name__ == '__main__':
-# ещё я попробовал сделать функцию с настройками
-# и вызвать её ниже но это не сработало, видимо так нельзя
+logger = logging.getLogger(__name__)
+StreamHandler = logging.StreamHandler()
+StreamHandler.setLevel(logging.DEBUG)
+FileHandler = RotatingFileHandler(
+    filename='program.log', maxBytes=50000000, backupCount=5)
+FileHandler.setLevel(logging.INFO)
+logger.addHandler(StreamHandler)
+logger.addHandler(FileHandler)
 
-logging.basicConfig(
-    format='%(asctime)s - %(levelname)s - %(message)s - %(lineno)s',
-    level=logging.DEBUG,
-    filename='program.log',
-    # hendlers=('program.log'),
-    encoding="UTF-8",
-    filemode='a'
-)
-
-
+BASE_LOG_DIR = 'program.log'
 PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
@@ -55,48 +44,55 @@ HOMEWORK_STATUSES = {
 def send_message(bot, message):
     """отправляет сообщение в Telegram чат."""
     try:
-        logging.info('Отправляем сообщение')
+        logging.info(f'Отправляем сообщение: {message}')
         bot.send_message(TELEGRAM_CHAT_ID, message)
-    except NotForSendException:
+    except NotForSendException as error:
+        logging.info(f'Сообщение не отправлено: {error}')
         pass
+    else:
+        logging.info('Сообщение не отправлено! Ура, товарищи!')
 
 
 def get_api_answer(current_timestamp):
     """делает запрос к эндпоинту API-сервиса."""
-    timestamp = current_timestamp or int(time.time())
-    params = {'from_date': timestamp}
-    logging.info('начали запрос к API яндкс.практикума')
-    response = requests.get(ENDPOINT, headers=HEADERS, params=params)
-    if response.status_code != HTTPStatus.OK:
-        logging.error(f'упал запрос к эндпойнту: {ENDPOINT}')
-        raise StatusCodeException(
+    try:
+        logging.info('начали запрос к API яндкс.практикума')
+        timestamp = current_timestamp or int(time.time())
+        params = {'from_date': timestamp}
+        dict_for_response = {
+            'url': ENDPOINT,
+            'headers': HEADERS,
+            'params': params,
+        }
+        response = requests.get(**dict_for_response)
+        if response.status_code != HTTPStatus.OK:
+            raise StatusCodeException
+    except StatusCodeException as error:
+        logging.info(
+            f'Запрос с параметрами {dict_for_response}, не прошёл'
             f'функции get_api_answer получила код: {response.status_code}'
-            f'какая то причина из респонса ???'
-            f'и какойто текст их контента ???'
+            f'Какова причина остановки?: {response.reason}'
+            f'ПРИЧИНА ОСТАНОВКИ??: {response.reason} !!!!'
+            f'Текст {response.text}'
+            f'ошибочка вышла: {error}'
         )
-    return response.json()
+    else:
+        logging.info('Шалость удалась, запрос от сервера получен')
+        return response.json()
 
 
 def check_response(response: dict) -> list:
+    # не понял про аннотацию
     """проверяет ответ API на корректность."""
     logging.info('начинаем проверку ответа от сервера')
-    # как тут isinstance применить я не придумал
-    # ведь нам нужно выбросить исключение в случае если тип НЕ словарь
-
-    # с этой проверкой на проходит пайтест
-
-    # if type(response) != dict:
-    #     raise TypeException(
-    #         'список домашек это не словарь')
+    if not isinstance(response, dict):
+        raise TypeError(
+            'список домашек это не словарь')
+    if 'homeworks' and 'current_date' not in response:
+        raise TypeException(
+            'homeworks или current_date нет в запросе')
     homework = response['homeworks']
-    # homework = response.get('homeworks')
-    # эта строчка не работает потому что
-    # AttributeError: 'list' object has no attribute 'get'
-    # хотя объект респонс это словарь а не список
-    pprint(homework)
-    print(type(homework))
-    # непонятно как через isinstance
-    if type(homework) != list:
+    if not isinstance(homework, list):
         raise KeyError(
             'под ключом `homeworks` домашки приходят не в виде списка')
     return homework
@@ -106,66 +102,81 @@ def parse_status(homework):
     """Извлекает статус домашней работы.
     и формулирует строку сообщения для отправки.
     """
-    # if not homework.get('homework_name'):
-    #     raise TypeException(
-    #         'нет названия у работы')
-    # Я ПОХОДУ НЕ ПОНИМАЮ ЧТО ЗНАЧИТ ВЫБРОСИТЬ ИСКЛЮЧЕНИЕ
     homework_name = homework.get('homework_name')
     homework_status = homework.get('status')
+    if 'homework_name' not in homework:
+        raise KeyError(
+            f'нет названия у работы {homework}')
     verdict = HOMEWORK_STATUSES[homework_status]
+    if homework_status not in HOMEWORK_STATUSES:
+        raise KeyError(
+            f'Статуса {homework_status} нет в словаре {HOMEWORK_STATUSES}')
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
 def check_tokens():
     """проверяет доступность переменных окружения."""
-    if all([PRACTICUM_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID]):
-        return True
-    return False
+    return all([PRACTICUM_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID])
 
 
 def main():
     """Основная логика работы бота."""
     if check_tokens() is False:
+        # Почему иф нот?  логика же такова:
+        # если Чек_токен вернул фолс
+        # значит отсутствуют токены, скажем об этом
         logging.critical('отсутствуют обязательные переменные окружения')
-        sys.exit()
+        sys.exit('отсутствуют обязательные переменные окружения')
+    current_report = {'name': '', 'messages': '', }
+    prev_report: dict = current_report.copy()
+    bot = telegram.Bot(token=TELEGRAM_TOKEN)
+    current_timestamp = int(time.time())
     while True:
-        current_report = {}
-        prev_report = {}
-        bot = telegram.Bot(token=TELEGRAM_TOKEN)
         try:
-            current_timestamp = int(time.time())
-            # как эта строчка может быть ниже респонса
-            # если в респонсе выполняется функция
-            # принимая current_timestamp как параметр
             response = get_api_answer(current_timestamp)
+            current_timestamp = response['current_date']
             homeworks = check_response(response)
-            if homeworks == []:
+            if homeworks:
+                homework = homeworks[0]
+                current_report['name'] = homework.get('homework_name')
+                message = parse_status(homework)
+                current_report['messages'] = message
+            else:
                 logging.info('Список домашних работ пуст')
-                # Если список домашек пустой, то шлём соответствующее
-                # сообщение. куда шлём? логируем то есть?
-                # или в телегу:
-                # send_message(bot, 'Список домашних работ пуст')
-            homework = homeworks[0]
-            current_report['name'] = homework.get('homework_name')
-            message = parse_status(homework)
-            current_report['messages'] = message
             if current_report != prev_report:
                 send_message(bot, message)
-                logging.info('сообщение успешно отправлено')
-            prev_report = current_report.copy()
-            time.sleep(RETRY_TIME)
+                prev_report = current_report.copy()
+            else:
+                logging.info('нет новых статусов')
         except NotForSendException:
             logging.error('Сообщение не отправлено')
-        except KeyError:
-            logging.error('в словаре нет такого ключа')
-        except IndexError:
-            logging.error('в списке нет запрошенного элемента')
-        # except Exception as error:
-        #     message = f'Сбой в работе программы: {error}'
-        #     logging.error(f'Сбой в работе программы: {error}')
+        # except KeyError:
+        #     logging.error('в словаре нет такого ключа')
+        # except IndexError:
+        #     logging.error('в списке нет запрошенного элемента')
+        except Exception as error:
+            message = f'Сбой в работе программы: {error}'
+            logging.error(f'Сбой в работе программы: {error}')
         finally:
             time.sleep(RETRY_TIME)
 
 
 if __name__ == '__main__':
+
+    logging.basicConfig(
+        format='%(asctime)s - %(levelname)s - %(message)s - %(lineno)s',
+        level=logging.INFO,
+        encoding="UTF-8",
+        handlers=[
+            RotatingFileHandler(
+                BASE_LOG_DIR,
+                mode='a',
+                # он вроде по умолчанию стоит
+                encoding="UTF-8",
+                maxBytes=5000000,
+                backupCount=5),
+            logging.StreamHandler(stream=sys.stdout),
+        ],
+    )
+
     main()
