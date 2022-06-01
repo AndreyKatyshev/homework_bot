d7d92c5d290d8183d52de1c537f856e88dd401b9
Андрей Катышев <Andrey-Katyshev@mail.ru>
2022-06-01 13:59:51 +0300

    после третьего ревью

--- homework.py
+++ homework.py
@@ -8,22 +8,14 @@ import time
 from dotenv import load_dotenv
 from http import HTTPStatus
 from logging.handlers import RotatingFileHandler
+from typing import Optional
 
 from exceptions import NotForSendException, StatusCodeException, TypeException
 
 
 load_dotenv()
 
-logger = logging.getLogger(__name__)
-StreamHandler = logging.StreamHandler()
-StreamHandler.setLevel(logging.DEBUG)
-FileHandler = RotatingFileHandler(
-    filename='program.log', maxBytes=50000000, backupCount=5)
-FileHandler.setLevel(logging.INFO)
-logger.addHandler(StreamHandler)
-logger.addHandler(FileHandler)
 
-BASE_LOG_DIR = 'program.log'
 PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
@@ -32,8 +24,6 @@ RETRY_TIME = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
 
-# здесь название словаря предлагалось по умолчанию,
-# я думаю что на нём тесты завязаны, но могу и пееименовать
 HOMEWORK_STATUSES = {
     'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
     'reviewing': 'Работа взята на проверку ревьюером.',
@@ -46,52 +36,56 @@ def send_message(bot, message):
     try:
         logging.info(f'Отправляем сообщение: {message}')
         bot.send_message(TELEGRAM_CHAT_ID, message)
-    except NotForSendException as error:
-        logging.info(f'Сообщение не отправлено: {error}')
-        pass
+    except telegram.error.TelegramError as error:
+        raise(f'Сообщение не отправлено {error}')
     else:
-        logging.info('Сообщение не отправлено! Ура, товарищи!')
+        logging.info('Сообщение отправлено! Ура, товарищи!')
 
 
 def get_api_answer(current_timestamp):
     """делает запрос к эндпоинту API-сервиса."""
+    timestamp = current_timestamp or int(time.time())
+    params = {'from_date': timestamp}
+    dict_for_response = {
+        'url': ENDPOINT,
+        'headers': HEADERS,
+        'params': params,
+    }
     try:
         logging.info('начали запрос к API яндкс.практикума')
-        timestamp = current_timestamp or int(time.time())
-        params = {'from_date': timestamp}
-        dict_for_response = {
-            'url': ENDPOINT,
-            'headers': HEADERS,
-            'params': params,
-        }
         response = requests.get(**dict_for_response)
         if response.status_code != HTTPStatus.OK:
-            raise StatusCodeException
-    except StatusCodeException as error:
-        logging.info(
-            f'Запрос с параметрами {dict_for_response}, не прошёл'
-            f'функции get_api_answer получила код: {response.status_code}'
-            f'Какова причина остановки?: {response.reason}'
-            f'ПРИЧИНА ОСТАНОВКИ??: {response.reason} !!!!'
-            f'Текст {response.text}'
-            f'ошибочка вышла: {error}'
-        )
-    else:
-        logging.info('Шалость удалась, запрос от сервера получен')
+            raise StatusCodeException(
+                f'Запрос с параметрами {dict_for_response}, не прошёл'
+                f'функции get_api_answer получила код: {response.status_code}'
+                f'Какова причина остановки?: {response.reason}'
+                f'Текст {response.text}'
+            )
         return response.json()
+    except Exception as error:
+        raise(
+            f'проблема с подключением:{error}'
+            f'Запрос выполняли вот с какими параметрами {dict_for_response}'
+        )
 
 
-def check_response(response: dict) -> list:
-    # не понял про аннотацию
+def check_response(response: Optional[dict]) -> Optional[list]:
+    # я перечитал тему аннотаций практикума,
+    # там мы импортируем из модуля тайпинг компановщики
+    # если переменная может принимать разные значения
+
+    # гуглил но я не понимаю что надо сделать
+    # я понял что версия пайтона у нас 3.7
+    # но что теперь с этим делать?
     """проверяет ответ API на корректность."""
     logging.info('начинаем проверку ответа от сервера')
     if not isinstance(response, dict):
         raise TypeError(
             'список домашек это не словарь')
-    if 'homeworks' and 'current_date' not in response:
+    if 'homeworks' not in response or 'current_date' not in response:
         raise TypeException(
             'homeworks или current_date нет в запросе')
-    homework = response['homeworks']
+    homework = response.get('homeworks')
     if not isinstance(homework, list):
         raise KeyError(
             'под ключом `homeworks` домашки приходят не в виде списка')
@@ -109,23 +103,30 @@ def parse_status(homework):
             f'нет названия у работы {homework}')
     verdict = HOMEWORK_STATUSES[homework_status]
     if homework_status not in HOMEWORK_STATUSES:
-        raise KeyError(
+        raise ValueError(
             f'Статуса {homework_status} нет в словаре {HOMEWORK_STATUSES}')
-    return f'Изменился статус проверки работы "{homework_name}". {verdict}'
+    return(
+        f'Изменился статус проверки работы "{homework_name}"'
+        f'вердикт {verdict}'
+        # f'вердикт {HOMEWORK_STATUSES[homework_status]}'
+
+        # f'вердикт: {0}'.format(HOMEWORK_STATUSES[homework_status])
+        # эта тоже не прошла.
+    )
 
 
 def check_tokens():
     """проверяет доступность переменных окружения."""
-    return all([PRACTICUM_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID])
+    return all([PRACTICUM_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID],)
 
 
 def main():
     """Основная логика работы бота."""
-    if check_tokens() is False:
-        # Почему иф нот?  логика же такова:
-        # если Чек_токен вернул фолс
-        # значит отсутствуют токены, скажем об этом
-        logging.critical('отсутствуют обязательные переменные окружения')
+    if not check_tokens():
+        logging.critical(
+            'отсутствуют обязательные переменные окружения:'
+            'PRACTICUM_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID'
+        )
         sys.exit('отсутствуют обязательные переменные окружения')
     current_report = {'name': '', 'messages': '', }
     prev_report: dict = current_report.copy()
@@ -142,7 +143,7 @@ def main():
                 message = parse_status(homework)
                 current_report['messages'] = message
             else:
-                logging.info('Список домашних работ пуст')
+                current_report['messages'] = 'нет новых статусов'
             if current_report != prev_report:
                 send_message(bot, message)
                 prev_report = current_report.copy()
@@ -150,12 +151,12 @@ def main():
                 logging.info('нет новых статусов')
         except NotForSendException:
             logging.error('Сообщение не отправлено')
-        # except KeyError:
-        #     logging.error('в словаре нет такого ключа')
-        # except IndexError:
-        #     logging.error('в списке нет запрошенного элемента')
         except Exception as error:
             message = f'Сбой в работе программы: {error}'
+            current_report['messages'] = message
+            if current_report != prev_report:
+                send_message(bot, message)
+                prev_report = current_report.copy()
             logging.error(f'Сбой в работе программы: {error}')
         finally:
             time.sleep(RETRY_TIME)
@@ -163,15 +164,16 @@ def main():
 
 if __name__ == '__main__':
 
+    BASE_LOG_DIR = 'program.log'
+
     logging.basicConfig(
         format='%(asctime)s - %(levelname)s - %(message)s - %(lineno)s',
-        level=logging.INFO,
+        level=logging.DEBUG,
         encoding="UTF-8",
         handlers=[
             RotatingFileHandler(
                 BASE_LOG_DIR,
                 mode='a',
-                # он вроде по умолчанию стоит
                 encoding="UTF-8",
                 maxBytes=5000000,
                 backupCount=5),
